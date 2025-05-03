3374c67842026e4ed2cc90994a0ba9a2e4ffd18a
FrereJOB <mbargagastonmagloire@gmail.com>
2025-05-03 19:50:24 +0100

    Update MainActivity.kt

--- app/src/main/java/com/jesusrevient/cantique/MainActivity.kt
+++ app/src/main/java/com/jesusrevient/cantique/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jesusrevient.cantique
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -9,20 +10,33 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val db = Firebase.firestore
-        val songsList = findViewById<ListView>(R.id.songs_list)
+        val db = Firebase.firestore
+        val songsList = findViewById<ListView>(R.id.songs_list)
 
-        db.collection("cantique").get()
-            .addOnSuccessListener { documents ->
-                val songs = documents.mapNotNull { it.toObject(Song::class.java).titre }
-                songsList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, songs)
-            }
-            .addOnFailureListener { e ->
-                e.printStackTrace()
-            }
-    }
+        db.collection("cantique").get()
+            .addOnSuccessListener { documents ->
+                val songs = documents.map { it.toObject(Song::class.java) }
+
+                val titles = songs.map { it.titre }
+                songsList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, titles)
+
+                songsList.setOnItemClickListener { _, _, position, _ ->
+                    val selectedSong = songs[position]
+                    val intent = Intent(this, SongDetailActivity::class.java).apply {
+                        putExtra("titre", selectedSong.titre)
+                        putExtra("auteur", selectedSong.auteur)
+                        putExtra("paroles", selectedSong.paroles)
+                        putExtra("categorie", selectedSong.categorie)
+                    }
+                    startActivity(intent)
+                }
+            }
+            .addOnFailureListener { e ->
+                e.printStackTrace()
+            }
+    }
 }
