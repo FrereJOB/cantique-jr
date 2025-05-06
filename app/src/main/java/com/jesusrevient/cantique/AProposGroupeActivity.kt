1d55f5ccd4cb3dd392ac87c00ce119fc878330ea
FrereJOB <mbargagastonmagloire@gmail.com>
2025-05-06 15:40:04 +0100

    Update AProposGroupeActivity.kt

--- app/src/main/java/com/jesusrevient/cantique/AProposGroupeActivity.kt
+++ app/src/main/java/com/jesusrevient/cantique/AProposGroupeActivity.kt
@@ -16,12 +16,30 @@ class AProposGroupeActivity : AppCompatActivity() {
         supportActionBar?.title = "À propos du groupe"
 
         binding.textInfoGroupe.text = """
-            Le groupe Jésus Revient est un ministère chrétien basé à Yaoundé, Cameroun. 
-            Il œuvre pour l’édification des croyants à travers la louange, l’adoration, 
-            la prédication et l’enseignement de la parole de Dieu.
-            
-            Nos cantiques sont inspirés par le Saint-Esprit pour fortifier la foi des enfants de Dieu.
-            Que Dieu vous bénisse abondamment !
+            **JESUS-REVIENT TV**  
+
+**Description**  
+Mouvement international d'évangélisation prêchant la repentance, la sanctification et la doctrine biblique pour préparer les saints à l'Enlèvement.  
+
+**Siège** :  
+Bénin, Dekoungbé, Hédomey (200m après la Pharmacie Hédomey).  
+**Tél.** : (+229) 97 25 35 39 / 94 33 69 05  
+**Chaîne TV** : "JESUS-REVIENT.TV" (Satellite Amos 17, décodeur Strong).  
+
+**Liens** :  
+- Facebook : facebook.com/groupejesusrevient  
+- Groupe FB : facebook.com/groups/jesusrevient.tv  
+- WhatsApp : whatsapp.com/channel/0029VaDKtIeHgZWfIv4S...  
+- Telegram : t.me/jesusrevient_tv  
+- Twitter : twitter.com/jesusrevienttv  
+- YouTube : youtube.com/@JESUSREVIENTTV  
+- Site : jesusrevient.tv  
+- GPS : maps.app.goo.gl/SFS46YG9JsMCuCW27  
+- Email : contact@jesusrevient.tv  
+- App Android : play.google.com/store/apps/details?id=com.mao...  
+
+---  
+*Version concise, conserve l'essentiel.* vous bénisse abondamment !
         """.trimIndent()
     }
 }
