80a687ab390640dfb44c770fd7137e97f2660d2a
FrereJOB <mbargagastonmagloire@gmail.com>
2025-05-06 16:26:55 +0100

    Update AProposGroupeActivity.kt

--- app/src/main/java/com/jesusrevient/cantique/AProposGroupeActivity.kt
+++ app/src/main/java/com/jesusrevient/cantique/AProposGroupeActivity.kt
@@ -26,20 +26,25 @@ Bénin, Dekoungbé, Hédomey (200m après la Pharmacie Hédomey).
 **Tél.** : (+229) 97 25 35 39 / 94 33 69 05  
 **Chaîne TV** : "JESUS-REVIENT.TV" (Satellite Amos 17, décodeur Strong).  
 
-**Liens** :  
-- Facebook : facebook.com/groupejesusrevient  
-- Groupe FB : facebook.com/groups/jesusrevient.tv  
-- WhatsApp : whatsapp.com/channel/0029VaDKtIeHgZWfIv4S...  
-- Telegram : t.me/jesusrevient_tv  
-- Twitter : twitter.com/jesusrevienttv  
-- YouTube : youtube.com/@JESUSREVIENTTV  
-- Site : jesusrevient.tv  
-- GPS : maps.app.goo.gl/SFS46YG9JsMCuCW27  
-- Email : contact@jesusrevient.tv  
-- App Android : play.google.com/store/apps/details?id=com.mao...  
 
----  
-*Version concise, conserve l'essentiel.* vous bénisse abondamment !
+<contenu>
+    <titre>Nous suivre</titre>
+    <liens>
+        <lien type="site" url="https://jesusrevient.tv" />
+        <lien type="facebook" url="https://facebook.com/groupejesusrevient" />
+        <lien type="Groupe Facebook" url="https:facebook.com/groups/jesusrevient.tv" />
+        <lien type="facebook" url="https://facebook.com/groupejesusrevient" />
+        <lien type="WhatsApp" url="https://wwww.whatsapp.com/channel/0029VaDKtleHgZWfIv4SNW3z" />
+        <lien type="Canal Telegram" url="https://t.me/jesusrevient" />
+        <lien type="Groupe Telegram" url="https://t.me/jesusrevient_tv" />
+        <lien type="Twitter" url="https://twitter.com/jesusrevienttv" />
+        <lien type="GPS" url="https://maps.app.goo.gl/SFS46YG9JsMCuCW27" />
+        <lien type="Application" url="play.google.com/store/apps/details?id=com.maougnonjesusrevient.jesus_revient_tv" />
+
+
+    </liens>
+</contenu>
+
         """.trimIndent()
     }
 }
