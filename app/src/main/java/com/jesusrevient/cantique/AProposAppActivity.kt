28cc138eec2db78f3a96bbae7bec7793bae6027e
FrereJOB <mbargagastonmagloire@gmail.com>
2025-05-06 05:36:41 +0100

    Create AProposAppActivity.kt

---
+++ app/src/main/java/com/jesusrevient/cantique/AProposAppActivity.kt
@@ -0,0 +1,29 @@
+package com.jesusrevient.cantique
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jesusrevient.cantique.databinding.ActivityAproposAppBinding
+
+class AProposAppActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAproposAppBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAproposAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "À propos de l'application"
+
+        binding.textInfoApp.text = """
+            Cette application est un recueil de chants chrétiens du groupe Jésus Revient.
+            
+            Elle a été développée par le frère JOB, membre de l'église Jésus Revient située à Yaoundé, Cameroun.
+            
+            Pour toute suggestion ou amélioration, vous pouvez contacter :
+            Email : mbargagastonmagloire@gmail.com
+            
+            Que le Seigneur vous bénisse richement !
+        """.trimIndent()
+    }
+}
