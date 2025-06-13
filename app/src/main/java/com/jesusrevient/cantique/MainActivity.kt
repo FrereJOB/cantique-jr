eeb4b94be417b1ea8b0e9cf337a8cdebf5b173e7
FrereJOB <mbargagastonmagloire@gmail.com>
2025-06-13 12:53:47 +0100

    Update MainActivity.kt

--- app/src/main/java/com/jesusrevient/cantique/MainActivity.kt
+++ app/src/main/java/com/jesusrevient/cantique/MainActivity.kt
@@ -15,11 +15,10 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.navigation.NavigationView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.jesusrevient.cantique.models.Song
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var recyclerView: RecyclerView
@@ -33,13 +32,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
 
         drawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        navView.setNavigationItemSelectedListener(this)
 
         val openDrawerButton: ImageButton = findViewById(R.id.open_drawer)
         val closeDrawerButton: ImageButton = findViewById(R.id.close_drawer)
@@ -101,38 +93,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             .addOnFailureListener { exception ->
                 Log.e("MainActivity", "Erreur lors du chargement des cantiques", exception)
             }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START)
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_group -> {
-                startActivity(Intent(this, AProposGroupeActivity::class.java))
-            }
-            R.id.nav_app -> {
-                startActivity(Intent(this, AProposAppActivity::class.java))
-            }
-            R.id.nav_admin -> {
-                startActivity(Intent(this, LoginAdminActivity::class.java))
-            }
-            R.id.nav_share -> {
-                val shareIntent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    putExtra(Intent.EXTRA_TEXT, "Téléchargez l'application Cantique JR sur votre téléphone !")
-                    type = "text/plain"
-                }
-                startActivity(Intent.createChooser(shareIntent, "Partager via"))
-            }
-        }
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
     }
 
     fun onGroupClick(view: View) {
