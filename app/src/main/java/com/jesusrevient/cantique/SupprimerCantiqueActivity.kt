afc22137ef76c3732f6cbcd1abaa9a47df802da3
FrereJOB <mbargagastonmagloire@gmail.com>
2025-06-25 07:59:59 +0100

    Update SupprimerCantiqueActivity.kt

--- app/src/main/java/com/jesusrevient/cantique/SupprimerCantiqueActivity.kt
+++ app/src/main/java/com/jesusrevient/cantique/SupprimerCantiqueActivity.kt
@@ -3,6 +3,7 @@ package com.jesusrevient.cantique
 import android.content.Intent
 import android.os.Bundle
 import android.widget.*
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
